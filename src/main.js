4aa8a53d2f073fc6fc092e4f72c8f3032129c78e
Hendrixer <willscottmoss@gmail.com>
2014-04-15 01:28:26 -0700

    (init) first commit

--- src/main.js
+++ src/main.js
@@ -190,3 +190,94 @@ var animate = angular.module('animations',
 
 );
 
+
+var app = angular.module('app', ['ngAnimate', 'animations']);
+
+app.controller('MainController', ['$scope', '$timeout', '$q', function($scope, $timeout, $q){
+
+  $scope.demo = {};
+  $scope.demo.cards = [];
+
+  $scope.demo.mainAnimation = null;
+  $scope.demo.animations = [
+    'fade-normal',
+    'fade-down',
+    'fade-down-big',
+    'fade-left',
+    'fade-left-big',
+    'fade-right',
+    'fade-right-big',
+    'fade-up',
+    'fade-up-big'
+  ];
+
+  $scope.demo.addCards = function(animation){
+    if($scope.demo.cards && $scope.demo.cards.length){
+      // $scope.demo.clean().then(function(){
+      //   $scope.demo.populate(animation);
+      // });
+      $scope.demo.cards = [];
+    }
+      $scope.demo.populate(animation);
+
+  };
+
+  $scope.demo.populate = function(animation){
+    $scope.demo.mainAnimation = animation;
+    var pushToCards = function(data){
+      return function (){
+        $scope.demo.cards.push({'header': data, 'type': animation});
+      };
+    };
+    var i   = 1,
+        end = 10;
+    for( ; i < end; i++){
+      $timeout(pushToCards('Item: '+i), i * 100);
+    }
+  };
+
+  $scope.demo.removeCard = function(index){
+    $scope.demo.cards.splice(index, 1);
+  };
+
+  $scope.demo.erase = function(){
+    $scope.demo.clean().then(function(){
+      $scope.demo.mainAnimation = null;
+    });
+  };
+
+  $scope.demo.clean = function(){
+    var dfrd = $q.defer();
+    var popCards = function(index){
+      return function(){
+        $scope.demo.cards.pop();
+        if(index >= 8){
+
+          dfrd.resolve(index);
+        }
+      };
+    };
+    angular.forEach($scope.demo.cards, function (card, index){
+      $timeout(popCards(index), 100 * index);
+    });
+    return dfrd.promise;
+  };
+
+}]);
+
+app.directive('card', function(){
+  return {
+    restrict: 'E',
+    scope: {
+      title: '@'
+    },
+    transclude: true,
+    replace: true,
+    template:
+    '<div class="card">'+
+      '<h4 class="card-header">{{ title }}</h4>'+
+      '<div class="card-content" ng-transclude></div>'+
+    '</div>'
+  };
+});
+
